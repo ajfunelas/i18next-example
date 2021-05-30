bd4e784e29f4013c00e5866c804c317cbe99b0e2
ajfunelas <aj_funelas@yahoo.com>
2021-05-31 00:18:16 +0800

    working example

--- localise-practice/src/App.tsx
+++ localise-practice/src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Trans, useTranslation } from 'react-i18next';
 
 function App() {
+  const {t, i18n} = useTranslation()
+
+  const changeLanguage = (language: any) => {
+    i18n.changeLanguage(language)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+      <option value="en" onClick={() => {changeLanguage("en")}}>EN</option>
+      <option value="jp" onClick={() => {changeLanguage("jp")}}>JP</option>
+      </select>
+      <button onClick={() => {changeLanguage("en")}}>EN</button>
+      <button onClick={() => {changeLanguage("jp")}}>JP</button>
+      <hr />
+       <Trans i18nKey="description.part1">
+         test 1
+         </Trans>
+         <div>{t("description.part2")}</div>
     </div>
   );
 }
