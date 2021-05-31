5847898a5af8316a6a03f99afa5408341dd81ed6
ajfunelas <aj_funelas@yahoo.com>
2021-05-31 23:47:03 +0800

    added translation parser

--- localise-practice/src/App.tsx
+++ localise-practice/src/App.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Trans, useTranslation } from 'react-i18next';
 
 function App() {
-  const {t, i18n} = useTranslation()
+	const { t, i18n } = useTranslation()
 
-  const changeLanguage = (language: any) => {
-    i18n.changeLanguage(language)
-  }
+	const changeLanguage = (language: any) => {
+		i18n.changeLanguage(language)
+	}
+
+	const [value, setValue] = useState("")
+
+	const options = [
+		{ value: "en", text: "English" },
+		{ value: "vi", text: "Vietnamese" },
+		{ value: "tg", text: "Tagalog" },
+	]
 
   return (
     <div className="App">
-      <select>
-      <option value="en" onClick={() => {changeLanguage("en")}}>EN</option>
-      <option value="jp" onClick={() => {changeLanguage("jp")}}>JP</option>
-      </select>
+      
       <button onClick={() => {changeLanguage("en")}}>EN</button>
       <button onClick={() => {changeLanguage("jp")}}>JP</button>
       <hr />
-       <Trans i18nKey="description.part1">
+       {/* <Trans i18nKey="description.part1">
          test 1
          </Trans>
-         <div>{t("description.part2")}</div>
+         <div>{t("description.part2")}</div> */}
+         <div>{t('this is a sentence')}</div>
+         <div>{t('how are you?')}</div>
     </div>
   );
 }
--- localise-practice/src/i18n.js
+++ localise-practice/src/i18n.js
@@ -1,19 +1,56 @@
-import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import { initReactI18next } from 'react-i18next'
+// import i18n from 'i18next'
+// import Backend from 'i18next-http-backend'
+// import LanguageDetector from 'i18next-browser-languagedetector'
+// import { initReactI18next } from 'react-i18next'
 
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'en',
-    debug: true,
-    detection: {
-        order: ['queryString','cookie'],
-        cache: ['cookie']
-    },
-    interpolation: {
-        escapeValue: false
-    }
-})
+// i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+//     fallbackLng: 'en',
+//     debug: true,
+//     detection: {
+//         order: ['queryString','cookie'],
+//         cache: ['cookie']
+//     },
+//     interpolation: {
+//         escapeValue: false
+//     }
+// })
+
+// export default i18n
+
+
+
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import jp from "./locales/jp/translation.json"
+import en from "./locales/en/translation.json"
+
+
+const resources = {
+	jp: {
+		translation: jp
+	},
+	en: {
+		translation: en
+},
+};
+
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources,
+		// debug: true,
+		fallbackLng: "en",
+		lng: "en",
+
+		// setting key and namespace separators to false, so that keys can just be the fallback text in english
+		keySeparator: false, // we do not use keys in form messages.welcome
+		namespaceSeparator: false,
+
+		interpolation: {
+			escapeValue: false, // react already safes from xss
+		},
+	})
+
 
 export default i18n
+
